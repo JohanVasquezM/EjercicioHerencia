fafb8afa612f3e265dca2c4eead6c117137fb47d
JohanVasquezM <vasquezmaldonadojohanisaac@gmail.com>
2024-05-06 13:54:52 -0600

    Ejecutando calculadora

--- EjercicioHerencia/Program.cs
+++ EjercicioHerencia/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioHerencia;
+
+Calculadora calculadora = new Calculadora();
+calculadora.Imprimir();
