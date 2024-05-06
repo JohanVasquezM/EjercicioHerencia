7076bb9b79cf12d70afc68e68994a506767bcd3e
JohanVasquezM <vasquezmaldonadojohanisaac@gmail.com>
2024-05-06 13:53:27 -0600

    Creando Operaciones

---
+++ EjercicioHerencia/Operaciones.cs
@@ -0,0 +1,22 @@
+﻿using EjercicioHerencia;
+
+namespace EjercicioHerencia
+{
+    public class Operaciones
+    {
+        public double CalcularAreaCuadrado(double lado)
+        {
+            return lado * lado;
+        }
+
+        public double CalcularVolumenCubo(double lado)
+        {
+            return lado * lado * lado;
+        }
+
+        public double CalcularLongitudCircunferencia(double radio)
+        {
+            return 2 * Math.PI * radio;
+        }
+    }
+}
