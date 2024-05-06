1c0d7ff049b5c1c403b1cd9781da6c5babe951f2
JohanVasquezM <vasquezmaldonadojohanisaac@gmail.com>
2024-05-06 13:54:08 -0600

    Clase Calculadora

---
+++ EjercicioHerencia/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using EjercicioHerencia;
+
+namespace EjercicioHerencia
+{
+    public class Calculadora : Operaciones
+    {
+        public void Imprimir()
+        {
+            double lado = 5.0;
+            double radio = 3.0;
+
+            Console.WriteLine("Área del cuadrado: " + CalcularAreaCuadrado(lado));
+            Console.WriteLine("Volumen del cubo: " + CalcularVolumenCubo(lado));
+            Console.WriteLine("Longitud de la circunferencia: " + CalcularLongitudCircunferencia(radio));
+        }
+    }
+}
